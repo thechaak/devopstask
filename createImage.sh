980afadde3ce3bfeb8244c2cf218df1f17f3d3d6
SRICHAKRAVARTHY <thechaak@gmail.com>
2022-05-17 02:38:11 +0530

    image tag updated

--- createImage.sh
+++ createImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 image="helloworld"
-version=$(date +"%m%d%Y-%H%M")
+version=$(date +"%D-%T")
 imagename="${image}:${version}"
 now="$(date +%s),"
 if [ -e sampledata.json ]
@@ -8,13 +8,8 @@ then
 	echo "changing the build time in sampledata.json"
 	bat="$(awk 'FNR == 3 { print $2 }' sampledata.json)"
 	sed -i "s_${bat}_${now}_g " sampledata.json
-       cat sampledata.json	
 else
 	echo "File not found! Please checkin sampledata.json"
 fi
-
-if [[ "$(docker image inspect ${imagename} --format='imageYes' 2> /dev/null)" == "imageYes" ]]; then
-	echo "${imagename} already exists!"
-else
 docker build -t ${imagename} .
-fi
+echo ${imagename} > delete
