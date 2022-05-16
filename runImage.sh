6325fbc48ea618aa0e1552d4e03a7293e44828e2
SRICHAKRAVARTHY <thechaak@gmail.com>
2022-05-17 02:18:28 +0530

    Removed checking the localhost port

--- runImage.sh
+++ runImage.sh
@@ -14,14 +14,8 @@ else
 	echo "File not found! Please checkin sampledata.json"
 fi
 sleep 1
-if [[ "$(nc -z 127.0.0.1 80 && echo 'IN USE' || echo 'FREE')" == "IN USE" ]]; then
-	echo "Port 80 is already in use, Please stop the application or Update this file to run the image on other port"
-	exit 0
-else
-	kubectl create deployment ${image} --image=${imagename}
-	kubectl expose deployment ${image} --type=NodePort --port=80	
-	## To run the same image through docker ##
-	##	docker run -it -d -p 80:80 --name helloworld ${imagename}
+kubectl create deployment ${image} --image=${imagename}
+kubectl expose deployment ${image} --type=NodePort --port=80	
+## To run the same image through docker ##
+##	docker run -it -d -p 80:80 --name helloworld ${imagename}
 echo "..Love from DevOps.."
-fi
-
